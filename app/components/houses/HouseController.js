1709ac9915447579ec334ebd8b116145c925b3e7
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-08-09 15:29:36 -0600

    commit final changes

--- app/components/houses/HouseController.js
+++ app/components/houses/HouseController.js
@@ -2,13 +2,43 @@ import HouseService from "./HouseService.js";
 
 let houseService = new HouseService
 
-export default class HouseController{
-    constructor(){
-
+function drawHouses() {
+    let houses = houseService.getHouses()
+    let template = ''
+    
+    for (let i = 0; i < houses.length; i++) {
+        const house = houses[i];
+        template += `
+        <div class="col-3">
+            <p>${house.name}</p>
+            <p>${house.bedroomQty}</p>
+            <p>${house.bathroomQty}</p>
+            <p>${house.houseType}</p>
+            <p>${house.levels}</p>
+            <p>${house.yearBuilt}</p>
+            <p>${house.price}</p>
+            <p>${house.imgUrl}</p>
+          </div>
+        `
+    }
+    
+    document.getElementById('houses').innerHTML = template
     }
 
 
 
-    
+export default class HouseController {
+    constructor() {
+        drawHouses()
+    }
+
+    addHouse(triggeredEvent) {
+        triggeredEvent.preventDefault();
+        let formData = triggeredEvent.target
+        houseService.addHouse(formData)
+        formData.reset()
+        drawHouses()
+
+    }
 }
 
--- app/components/houses/HouseService.js
+++ app/components/houses/HouseService.js
@@ -2,12 +2,47 @@ import House from '../../models/House.js'
 
 let houses = []
 
-export default class HouseService{
-    constructor(){
-        
+export default class HouseService {
+    constructor() {
+
+    }
+
+    getHouses() {
+        let housesCopy = []
+        houses.forEach(house => {
+            housesCopy.push(new House(
+                house.name,
+                house.bedroomQty,
+                house.bathroomQty,
+                house.houseType,
+                house.levels,
+                house.yearBuilt,
+                house.price,
+                house.imgUrl
+            ))
+        })
+        return housesCopy
     }
 
 
 
+    yearBuilt
+    price
+    imgUrl
+
+    addHouse(formData) {
+        let newHouse = new House(
+            formData.name.value,
+            formData.bedroomQty.value,
+            formData.bathroomQty.value,
+            formData.houseType.value,
+            formData.levels.value,
+            formData.yearBuilt.value,
+            formData.price.value,
+            formData.imgUrl.value
+        )
+        houses.push(newHouse)
+        console.log(houses)
+    }
 
 }
